ec7bf0a06acc2ccf75eb242481709d7796e466cc
hyoungsun <hyoungsun@uchicago.edu>
2018-01-01 22:38:10 -0600

    Initial Commit - Set up Snapkit (install Pod)

---
+++ PTalk/LoginViewController.swift
@@ -0,0 +1,63 @@
+//
+//  LoginViewController.swift
+//  PTalk
+//
+//  Created by HYOUNGSUN park on 1/1/18.
+//  Copyright © 2018 com.stanleypark. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBOutlet weak var signup: UIButton!
+    
+    let remoteconfig = RemoteConfig.remoteConfig()
+    var color: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let statusBar = UIView()
+        
+        self.view.addSubview(statusBar)
+        statusBar.snp.makeConstraints{ (m) in
+            m.right.top.left.equalTo(self.view)
+            m.height.equalTo(20)
+        }
+        
+        color = remoteconfig["splash_background"].stringValue
+        
+        statusBar.backgroundColor = UIColor(hex: color)
+        loginButton.backgroundColor = UIColor(hex: color)
+        signup.backgroundColor = UIColor(hex: color)
+
+        signup.addTarget(self, action: #selector(presentSignup), for: .touchUpInside)
+        // Do any additional setup after loading the view.
+    }
+    
+    @objc func presentSignup() {
+        let view = self.storyboard?.instantiateViewController(withIdentifier: "SignupViewController") as! SignupViewController
+        
+        self.present(view, animated: true, completion: nil)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'PTalk' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for PTalk
+
+pod 'SnapKit', '~> 4.0.0'
+pod 'Firebase/Core'
+pod 'Firebase/RemoteConfig'
+pod 'TextFieldEffects'
+pod 'Firebase/Database'
+pod 'Firebase/Auth'
+
+end
